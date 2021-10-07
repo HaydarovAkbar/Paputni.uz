fb56362564e5babacc09d085444f86c1469fea49
Akbar Haydarov <81344759+Akbarpython@users.noreply.github.com>
2021-10-07 21:06:39 +0500

    Update settings.py
    xatoliklar tugirlanadi

--- Paputni_project/Paputni_project/settings.py
+++ Paputni_project/Paputni_project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Paputni_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
